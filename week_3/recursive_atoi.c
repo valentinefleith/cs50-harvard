d6dccab9268a7913661b9e436d811f9f1131f11c
valentinefleith <valentine.fleith@gmail.com>
2023-06-06 11:35:20 +0200

    recursive atoi

---
+++ week_3/recursive_atoi.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int convert(string input);
+
+int main(void)
+{
+    string input = get_string("Enter a positive integer: ");
+
+    for (int i = 0, n = strlen(input); i < n; i++)
+    {
+        if (!isdigit(input[i]))
+        {
+            printf("Invalid Input!\n");
+            return 1;
+        }
+    }
+
+    // Convert string to int
+    printf("%i\n", convert(input));
+}
+
+int convert(string input)
+{
+    if (strlen(input) == 0)
+    {
+        return 0;
+    }
+
+    int num = input[strlen(input) - 1] - '0';
+    input[strlen(input) - 1] = '\0';
+    return num + 10 * convert(input);
+}
